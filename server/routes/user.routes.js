5a06694c6f86b6d8762aa0e1a7adb06f47637385
SenchaBka <aburiak@my.centennialcollege.ca>
2023-11-12 20:09:38 -0500

    Remove not used code and files

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,10 +5,10 @@ import userCtrl from '../controllers/user.controller.js'
 	router.route('/api/users') 
 	.get(userCtrl.list)
 	.post(userCtrl.create)
-	/*router.route('/api/users/:userId') 
+	router.route('/api/users/:userId') 
 	.get(userCtrl.read)
 	.put(userCtrl.update) 
-	.delete(userCtrl.remove)*/
+	.delete(userCtrl.remove)
 	router.route('/api/users/:userId')
 .get(authCtrl.requireSignin, userCtrl.read)
 .put(authCtrl.requireSignin, authCtrl.hasAuthorization, 
