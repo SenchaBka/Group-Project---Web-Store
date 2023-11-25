69cc783fb8bacfbc66cf319877fde6d49b0ac306
SemiMochiBalls <123606903+SemiMochiBalls@users.noreply.github.com>
2023-11-25 16:35:21 -0500

    Add dependencies and update code
    added login and register pages. logic still testing

--- server/controllers/error.controller.js
+++ server/controllers/error.controller.js
@@ -1,12 +1,12 @@
 function handleError(req, res) {
- //code to handle the error
-    
-}
-function getErrorMessage(errMsg) {
-console.log(errMsg);
+    // code to handle the error
 }
 
-export default  {
+function getErrorMessage(errMsg) {
+    console.log(errMsg);
+}
+
+export default {
     handleError: handleError,
-    getErrorMessage:getErrorMessage
+    getErrorMessage: getErrorMessage
 };
