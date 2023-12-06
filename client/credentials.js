f49f0d546d7d585fb3af80b68ceacfc8a1f8d1b9
SenchaBka <aburiak@my.centennialcollege.ca>
2023-12-06 12:31:48 -0500

    Add separate file with firebase config

---
+++ client/credentials.js
@@ -0,0 +1,10 @@
+//Web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAve3Kdp7vBhqt0E6BA0fWpbOSCxIqOCls",
+    authDomain: "week11-1-6c138.firebaseapp.com",
+    projectId: "week11-1-6c138",
+    storageBucket: "week11-1-6c138.appspot.com",
+    messagingSenderId: "753661187204",
+    appId: "1:753661187204:web:94e664f28ed4119a0e9ed1",
+    measurementId: "G-LK84KM2HT8"
+};
