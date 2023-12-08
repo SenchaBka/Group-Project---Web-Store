a7df788c9f3ecf9a1f501e9106e44d9ebf99acc1
SenchaBka <aburiak@my.centennialcollege.ca>
2023-12-08 00:32:39 -0500

    Add sign up page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./firebase_setup/Signin";
 import Profile from "./firebase_setup/Profile";
+import Signup from "./firebase_setup/Singup";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
@@ -13,6 +14,7 @@ root.render(
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
                 <Route path="profile" element={<Profile />} />
             </Routes>
         </BrowserRouter>
