c1c56aba156724e93cbacd225be22f96dd4c0f00
SenchaBka <aburiak@my.centennialcollege.ca>
2023-12-09 12:37:38 -0500

    Remove products functionality and add sign out

--- src/firebase_setup/Profile.js
+++ src/firebase_setup/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Navigation from '../components/Navigation';
-import { auth, changeEmail, changePassword, addNewItem, showUserItems, updateItem } from './firebase';
+import { auth, changeEmail, changePassword } from './firebase';
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
   const [newEmail, setNewEmail] = useState('');
@@ -8,11 +9,6 @@ const Profile = () => {
   const [newPassword, setNewPassword] = useState('');
   const [error, setError] = useState('');
   const [currentEmail, setCurrentEmail] = useState('');
-
-  const [newItem, setNewItem] = useState('');
-  const [itemName, setItemName] = useState('');
-  const [itemPrice, setItemPrice] = useState('');
-  const [itemDesc, setItemDesc] = useState('');
 
   useEffect(() => {
     // Fetch the current user's email when the component mounts
@@ -23,7 +19,20 @@ const Profile = () => {
   }, []);
 
   const handleLogout = async () => {
-    // Your existing logout logic
+    var userEmail = document.getElementById('userEmail');
+    var messageElement = document.getElementById('signOutMessage');
+    
+    try {
+      await auth.signOut();
+      userEmail.textContent = "Not logged in";
+
+      messageElement.textContent = 'User signed out successfully';
+      messageElement.style.color = 'green';
+
+    } catch (error) {
+      messageElement.textContent = 'Error signing out: ' + error.message;
+      messageElement.style.color = 'red';
+    }
   };
 
   return (
@@ -31,12 +40,11 @@ const Profile = () => {
       <body>
         <Navigation />
         <h1>Profile</h1>
-        <p>Current Email: {currentEmail}</p>
+        <p id='userEmail'>Current Email: {currentEmail}</p>
         {error && <p style={{ color: 'red' }}>{error}</p>}
-        <p id="userEmail"></p>
         <input type="email" id="newEmail" placeholder="Enter new email" required />
         <input type="password" id="currentPassword" placeholder="Enter your password" required />
-        <button onClick={() => changeEmail(newEmail, password, setCurrentEmail)}>Update Email</button>
+        <button onClick={() => changeEmail(newEmail, password)}>Update Email</button>
         <p id="emailChangeMessage"></p>
 
         <input type="password" id="password" placeholder="Enter current password" required />
@@ -44,31 +52,8 @@ const Profile = () => {
         <button onClick={() => changePassword(password, newPassword)}>Update Password</button>
         <p id="passwordChangeMessage"></p>
 
-        <section id='addItemForm'>
-          <h2>Add Item</h2>
-          <label htmlFor="itemName">Item Name:</label><br />
-          <input type="text" id="itemName" name="itemName" required /><br />
-          <label htmlFor="itemPrice">Item Description:</label><br />
-          <input type="text" id="itemDesc" name="itemDesc" required /><br />
-          <label htmlFor="itemPrice">Item Price:</label><br />
-          <input type="text" id="itemPrice" name="itemPrice" required /><br />
-          <button onClick={() => addNewItem()}>Add Item</button>
-          <p id="itemAddMessage"></p>
-        </section>
-
-        <section id='editItemForm' style={{ display: 'none' }}>
-        <h2>Edit Item</h2>
-          <label htmlFor="editItemName">New Item Name:</label><br />
-          <input type="text" id="editItemName" name="editItemName" required /><br />
-          <label htmlFor="editItemDesc">New Item Description:</label><br />
-          <input type="text" id="editItemDesc" name="editItemDesc" required /><br />
-          <label htmlFor="editItemPrice">New Item Price:</label><br />
-          <input type="text" id="editItemPrice" name="editItemPrice" required /><br />
-        </section>
-
-        <h2>Your Products:</h2>
-        <ul id="items-list"></ul>
-        <button onClick={() => showUserItems(password, newPassword)}>Show your products</button>
+        <button onClick={() => handleLogout(setCurrentEmail)}>Sign Out</button>
+        <p id="signOutMessage"></p>
       </body>
     </>
   );
