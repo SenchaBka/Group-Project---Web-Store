5c76e433e5fed20c037e603682e57d7521d77457
SenchaBka <aburiak@my.centennialcollege.ca>
2023-12-09 12:24:04 -0500

    Add link to products page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./firebase_setup/Signin";
 import Profile from "./firebase_setup/Profile";
 import Signup from "./firebase_setup/Singup";
+import Products from "./firebase_setup/Products";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
@@ -16,6 +17,7 @@ root.render(
                 <Route path="login" element={<Login />} />
                 <Route path="signup" element={<Signup />} />
                 <Route path="profile" element={<Profile />} />
+                <Route path="products" element={<Products />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
